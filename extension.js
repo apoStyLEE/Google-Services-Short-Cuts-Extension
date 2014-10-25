c56969a47c496c388ef142710b5e63f17960cfb1
Abdullah Uğraşkan <ugraskan@gmail.com>
2014-10-25 17:07:53 +0300

    first commit

---
+++ extension.js
@@ -0,0 +1,51 @@
+var contexts = ["selection"];
+
+// search text Youtube
+chrome.contextMenus.create({
+  title: "Search '%s' Youtube", 
+  contexts:contexts, 
+  onclick: function(info, tab){ 
+    var selectionText = info.selectionText;
+    var youtubeSearchUrl = "http://www.youtube.com/results?search_query="+selectionText;
+    createTab(youtubeSearchUrl);    
+  }
+});
+
+// send text Translate
+chrome.contextMenus.create({
+  title: "Translate '%s' to Turkish", 
+  contexts:contexts, 
+  onclick: function(info, tab){
+    var selectionText = info.selectionText;
+    var translateTextUrl = "https://translate.google.com.tr/?hl=tr#en/tr/"+selectionText;
+    createTab(translateTextUrl);   
+  }
+});
+
+// send link Translate
+chrome.contextMenus.create({
+  title: "Translate selected link to Turkish", 
+  contexts:["link"], 
+  onclick: function(info, tab){
+    var translateLinkUrl = "https://translate.google.com.tr/translate?sl=en&tl=tr&js=y&prev=_t&hl=tr&ie=UTF-8&u="+info.linkUrl+"&edit-text=&act=url";
+    createTab(translateLinkUrl);  
+  }
+});
+
+// send Gmail
+chrome.contextMenus.create({
+  title: "Send selected text Gmail", 
+  contexts:contexts, 
+  onclick: function(info, tab){
+    var selectionText = info.selectionText;
+    var pageUrl = info.pageUrl;
+    var mailBody = selectionText +" - "+ pageUrl;
+    var translateUrl = "https://mail.google.com/mail/u/0/?view=cm&fs=1&body="+mailBody+"&tf=1";
+    createTab(translateUrl);
+  }
+});
+
+// common funcs
+function createTab(url) {
+  chrome.tabs.create({"url" : encodeURI(url) });
+}
